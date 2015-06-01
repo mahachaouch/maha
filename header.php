cf462aa89522b58e629fbc0652cec5d6bb19f872
maha <maha-chaouch@hotmail.fr>
2015-06-01 20:16:08 +0200

    test7

--- header.php
+++ header.php
@@ -20,6 +20,8 @@ $bdd = new Database("127.9.150.130", "maha", "adminWzZH6pH", "u_bKWtXEbEAS");
   <link rel="stylesheet" href="styles/main.css">
   
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 </head>
 <body>
 
